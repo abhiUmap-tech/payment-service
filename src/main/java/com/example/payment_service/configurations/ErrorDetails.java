c5e45e1069b8a77257af250b78aa2d35bbf17c1f
abhiUmap-tech <abhiumap31@gmail.com>
2025-03-19 19:46:30 +0530

    add error details

---
+++ src/main/java/com/example/payment_service/configurations/ErrorDetails.java
@@ -0,0 +1,20 @@
+package com.example.payment_service.configurations;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private int status;
+
+    private String message;
+
+    private LocalDateTime timeStamp;
+
+}
